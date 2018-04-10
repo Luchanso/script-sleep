8ecfaeaa0200928f6c8af7957d983f40106341ab
Luchansky Oleg <luchansodev@gmail.com>
2018-04-10 21:53:10 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+Этот скрипт помогает вам заснуть
+
+This script helps you fall asleep
+
+Requirements: MacOS + Home Brew
---
+++ sleep.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+open https://www.noisli.com/get_combo_by_link/2BtCt43QB6M4dvC
+brightness 0.063477
+m volume 19
+m nosleep until 2000 && m sleep
